130311aeaaea760588332656f866019445e3c816
nhkhanh068 <nhkhanh068@gmail.com>
2017-02-20 15:45:58 +0700

    edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, :find_user, except: [:show, :create, :new]
+  before_action :correct_user, :find_user, only: [:edit, :update, :show]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.per_page
+  end
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find_by(params[:id])
   end
 
   def create
@@ -17,9 +24,57 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id]).destroy unless @user
+    flash[:success] = t "user_deleted"
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "pls_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    find_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    render_404 unless @user
+  end
+
+  def render_404
+   respond_to do |format|
+    format.html{render file: "#{Rails.root}/public/404", layout: false, status: :not_found}
+    format.xml{head :not_found}
+    format.any{head :not_found}
+   end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: Settings.email_max_length},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.password_min_length}
+  validates :password, presence: true, length: {minimum: Settings.password_min_length},
+    allow_nil: true
 
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
